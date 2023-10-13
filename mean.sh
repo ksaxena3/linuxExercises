4c55f6f62168259fc57782fe1aa89d37b2bdd9f0
Kanishk Saxena <kanishksaxena5@gmail.com>
2023-10-13 16:05:15 -0500

    Complete step-7

--- mean.sh
+++ mean.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+    echo "Usage: $0 <column> [file.csv]" >&2
+    exit 1
+fi
+
+# Get the column number
+column="$1"
+
+# Determine the input source (file or stdin)
+if [ "$#" -eq 2 ]; then
+    file="$2"
+else
+    file="/dev/stdin"
+fi
+
+# Check if the file exists (except for /dev/stdin)
+if [ "$file" != "/dev/stdin" ] && [ ! -f "$file" ]; then
+    echo "Error: File '$file' not found." >&2
+    exit 1
+fi
+
+# Calculate the mean
+mean=$(cut -d ',' -f "$column" "$file" | tail -n +2 | { sum=0; count=0; while read -r value; do sum=$((sum + value)); count=$((count + 1)); done; echo "scale=2; $sum / $count" | bc; })
+
+# Print the mean
+if [ "$file" != "/dev/stdin" ]; then
+    echo "Mean of column $column in '$file': $mean"
+else
+    echo "Mean of column $column from stdin: $mean"
+fi
+
