49d7f289aedd7f09e6b399ba6b966e0a9a1d7a6b
philip.lin.1234@gmail.com <philip.lin.1234@gmail.com>
2023-10-13 21:33:22 -0500

    Sum of numbers between 1000 and 2000

--- digits.sh
+++ digits.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+for number in {1000..2000}; do
+    if [[ $number =~ ^[01]+$ ]]; then
+        sum=$((sum + number))
+    fi
+done
+
+echo "Sum of numbers with only 0s and 1s between 1000 and 2000: $sum"
