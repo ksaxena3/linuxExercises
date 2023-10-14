a10b0638601d3b639fd98b8f0283d0f3e8e06b97
Srivats <skumar237@wisc.edu>
2023-10-13 23:06:33 -0500

    Finished writing the shell script.

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 1
+fi
+
+dir="$1"
+size="$2"
+
+if [ ! -d "$dir" ]; then
+    echo "Error: Directory '$dir' not found." 1>&2
+    exit 1
+fi
+
+find "$dir" -type f -size +${size}c -exec rm {} \;
+
+echo "Files larger than ${size} bytes removed successfully from '$dir'."
