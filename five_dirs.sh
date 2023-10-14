f99625e51bf3c4ee03c7fcf2110904c0d608df8e
Srivats <skumar237@wisc.edu>
2023-10-13 23:05:01 -0500

    Finished writing the shell script.

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+mkdir five
+
+for i in {1..5}; do
+    dir="five/dir$i"
+    mkdir -p "$dir"
+    for j in {1..4}; do
+        for k in $(seq $j); do
+            echo $j >> "$dir/file$j"
+        done
+    done
+done
