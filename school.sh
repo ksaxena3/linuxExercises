862bb91f3a3056f606bfb74f7d8179f5506e5799
philip.lin.1234@gmail.com <philip.lin.1234@gmail.com>
2023-10-13 21:29:24 -0500

    Add school.sh script for average TotalAssesedValue column

--- school.sh
+++ school.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d, -f7 | {
+    sum=0
+    count=0
+    while IFS= read -r line; do
+        sum=$((sum + line))
+        count=$((count + 1))
+    done
+
+    if [ $count -eq 0 ]; then
+        echo "No properties found in MADISON SCHOOLS district."
+    else
+        average=$((sum / count))
+        echo "Average TotalAssessedValue for MADISON SCHOOLS district: $average"
+    fi
+}
